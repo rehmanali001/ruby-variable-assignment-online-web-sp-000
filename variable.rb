7eb4eb5a6c7aa53e50a327113f045addb07f88fe
Rehman Ali <rehmanali001@gmail.com>
2019-07-03 14:07:10 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,5 +1,5 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-example = "The word 'example' is equal to this sentence, it's a named variable."
+greeting = "Hello World!"
  
-puts example
+puts greeting
